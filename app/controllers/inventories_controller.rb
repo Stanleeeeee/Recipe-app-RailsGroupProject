19eedcf1b10aa3245cdb57df6e1f7d9b8a309899
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-09-21 01:27:21 +0200

    Generated inventory controller with index, show, and new methods

---
+++ app/controllers/inventories_controller.rb
@@ -0,0 +1,13 @@
+class InventoriesController < ApplicationController
+  def index
+    @inventories = Inventory.where(user_id: current_user.id)
+  end
+
+  def show
+    @inventory = Inventory.find(params[:id])
+  end
+
+  def new
+    @inventory = Inventory.new
+  end    
+end
