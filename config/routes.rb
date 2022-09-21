d3d2b2e056324da2e35134b4c26b129e0a92afc8
brahimdidi <brahimgb1team@gmail.com>
2022-09-21 17:35:00 +0000

    set recipes#public as root of the app, remove relations in routes resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :recipes, :except => [:update]
-    resources :foods, :except => [:update]
-  end
+  resources :users
+  resources :recipes, except: :update
+  resources :foods, except: :update
   # Defines the root path route ("/")
-  root "users#index"
+  root "recipes#public"
 end
