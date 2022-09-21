c653368edbe4f9823e8b157b50e29b0b2c15597b
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-09-21 17:05:29 +0100

    Add method classes and validations to foods in model

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,14 +5,3 @@ class Food < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 250 }
   validates :measurement_unit, presence: true, length: { minimum: 1, maximum: 250 }
   validates :price, presence: true, numericality: { greater_than: 0 }
-
-  def update_price(price)
-    self.price = price
-    save
-  end
-
-  def update_measurement_unit(measurement_unit)
-    self.measurement_unit = measurement_unit
-    save
-  end
-end
