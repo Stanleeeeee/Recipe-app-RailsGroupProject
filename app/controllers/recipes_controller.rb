27bf83fd40db2c4fd3f7678e4e5092b35cad873c
brahimdidi <brahimgb1team@gmail.com>
2022-09-21 17:41:02 +0000

    fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,15 @@
 class RecipesController < ApplicationController
   def index
-    unless current_user.nil?
-      @current_user = current_user
-      @recipes = @current_user.recipes
-    end
+    return if current_user.nil?
+
+    @current_user = current_user
+    @recipes = @current_user.recipes
   end
-  def show 
+
+  def show
     @recipe = Recipe.find(params[:id])
   end
+
   def public
     @recipes = Recipe.where(public: true)
   end
