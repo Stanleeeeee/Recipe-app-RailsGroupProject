0529313db6cd698d94486f863edd7c4cb1334e6f
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-09-22 13:39:43 +0200

    Add private compare, sum, set shopping list and params  methods in  shopping list controller

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -18,4 +18,31 @@ class ShoppingListsController < ApplicationController
       end
     end
   end
+
+  private
+
+  def compare
+    @inventory = Inventory.find(params[:inventory_id])
+    inv_foods = @inventory.related_inventory_foods.pluck(:food_id)
+    rec_foods = @recipe.related_recipe_foods
+    list = []
+    rec_foods.each do |item|
+      list.push(item) unless inv_foods.include? item.food_id
+    end
+    list
+  end
+
+  def sum(list)
+    total = 0
+    list.each { |item| total += item.food.price }
+    total
+  end
+
+  def set_shopping_list
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+
+  def shopping_list_params
+    params.require(:shopping_list).permit(:inventory_id)
+  end
 end
