214a2b4bde61f0c221dd97c66f66fbd18a077e52
brahimdidi <brahimgb1team@gmail.com>
2022-09-24 13:19:45 +0000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users
-  resources :recipes do
+  resources :recipes, except: :update do
     resources :recipe_foods
   end
   resources :foods, except: :update
